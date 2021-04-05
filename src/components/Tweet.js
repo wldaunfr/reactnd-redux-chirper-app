dbc390b7e172348407aa8e4b323347b623380a0b
Daniel Unfried <du@validio.de>
2021-04-05 17:25:10 +0200

    Implement liking a Tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,11 +2,18 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {formatDate, formatTweet} from '../utils/helpers';
 import {TiArrowBackOutline, TiHeartFullOutline, TiHeartOutline} from 'react-icons/lib/ti';
+import {handleToggleTweet} from '../actions/tweets';
 
 class Tweet extends Component {
   handleLike = (e) => {
     e.preventDefault();
-    // todo: Handle Like Tweet.
+
+    const {dispatch, tweet, authedUser} = this.props;
+    dispatch(handleToggleTweet({
+      id: tweet.id,
+      hasLiked: tweet.hasLiked,
+      authedUser,
+    }));
   };
 
   toParent = (e, id) => {
