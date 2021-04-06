a246020885e65ccac258102417c262c351d64556
Daniel Unfried <du@validio.de>
2021-04-06 09:41:40 +0200

    Add TweetPage Component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import {handleInitialData} from '../actions/shared';
 import Dashboard from './Dashboard';
 import NewTweet from './NewTweet';
 import LoadingBar from 'react-redux-loading';
+import TweetPage from './TweetPage';
 
 class App extends Component {
   componentDidMount() {
@@ -16,7 +17,7 @@ class App extends Component {
         <LoadingBar/>
         {this.props.loading === true
           ? null
-          : <NewTweet/>
+          : <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}}/>
         }
       </div>
     );
