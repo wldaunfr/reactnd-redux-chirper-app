af89c17b2886b723d055dff88e741da3a5c70f53
Daniel Unfried <du@validio.de>
2021-04-06 10:27:30 +0200

    Add React Router

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {handleAddTweet} from '../actions/tweets';
+import {Redirect} from 'react-router-dom';
 
 class NewTweet extends Component {
   state = {
     text: '',
+    toHome: false,
   };
 
   handleChange = (e) => {
@@ -24,14 +26,15 @@ class NewTweet extends Component {
 
     this.setState(() => ({
       text: '',
+      toHome: !id,
     }));
   };
 
   render() {
-    const {text} = this.state;
+    const {text, toHome} = this.state;
 
-    {
-      /* todo: Redirect to / if submitted */
+    if (toHome) {
+      return <Redirect to='/'/>;
     }
 
     const tweetLeft = 280 - text.length;
